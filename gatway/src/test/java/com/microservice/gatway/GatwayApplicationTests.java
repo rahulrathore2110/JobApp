588f7191fd52e1eb08194c02f939003059bb8eb6
Rahul Rathore <rahulg27727@gmail.com>
2024-03-04 22:34:22 +0530

    Api Gateway Setup

---
+++ gatway/src/test/java/com/microservice/gatway/GatwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.gatway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
